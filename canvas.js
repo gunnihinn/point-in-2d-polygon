8f93f99e02641863cdbb2bcbe1b68c58aaac1a73
Gunnar Þór Magnússon <gunnarm@tern.is>
2023-01-19 21:37:01 +0000

    Ray casting ftw

--- canvas.js
+++ canvas.js
@@ -2,37 +2,63 @@ var drawPoints = false;
 var points = [];
 
 function normals(points) {
-	ns = [];
+        ns = [];
 
-	for (i = 0; i < points.length - 1; i++) {
-		const [x0, y0] = points[i];
-		const [x1, y1] = points[i+1];
-		const x = x1 - x0;
-		const y = y1 - y0;
-		ns.push([y, -x]);
-	}
+        for (i = 0; i < points.length - 1; i++) {
+                const [x0, y0] = points[i];
+                const [x1, y1] = points[i + 1];
+                const x = x1 - x0;
+                const y = y1 - y0;
+                ns.push([y, -x]);
+        }
 
-	return ns;
+        return ns;
 }
 
 function scalar(p, q) {
-	var s = 0;
-	for (let i = 0; i < p.length; i++) {
-		s += p[i] * q[i];
-	}
-	return s;
+        var s = 0;
+        for (let i = 0; i < p.length; i++) {
+                s += p[i] * q[i];
+        }
+        return s;
 }
 
-function isInsideConvex(x, y) {
-	ns = normals(points);
+function isInsideConvex(xy, points) {
+        ns = normals(points);
 
-	for (let i = 0; i < points.length - 1; i++) {
-		if (scalar(points[i], ns[i]) > scalar([x, y], ns[i])) {
-			return false;
-		}
-	}
+        for (let i = 0; i < points.length - 1; i++) {
+                if (scalar(points[i], ns[i]) > scalar(xy, ns[i])) {
+                        return false;
+                }
+        }
 
         return true;
+}
+
+function isInsideGeneral(xy, points) {
+        const ns = normals(points);
+        const dir = ns[0];
+        var count = 0;
+
+        for (let i = 0; i < points.length - 1; i++) {
+                const p1 = points[i];
+                const p2 = points[i + 1];
+                const n = ns[i];
+                const lambda = (scalar(p1, n) - scalar(xy, n)) / scalar(dir, n);
+
+                const v = [
+                        xy[0] + lambda * dir[0] - p1[0],
+                        xy[1] + lambda * dir[1] - p1[1],
+                ];
+                const delta = [p2[0] - p1[0], p2[1] - p1[1]];
+                const t = scalar(v, delta) / scalar(delta, delta);
+
+                if (lambda >= 0 && 0 <= t && t <= 1) {
+                        count += 1;
+                }
+        }
+
+        return count % 2 == 1;
 }
 
 function getXY(event) {
@@ -43,7 +69,7 @@ function getXY(event) {
         const x = event.clientX - box.left;
         const y = event.clientY - box.top;
 
-	console.log(`x=${x}, y=${y}`);
+        console.log(`x=${x}, y=${y}`);
 
         return [x, y];
 }
@@ -58,7 +84,8 @@ function point(event) {
         if (drawPoints) {
                 ctx.beginPath();
                 ctx.arc(x, y, r, 0, 2 * Math.PI, false);
-                if (isInsideConvex(x, y)) {
+                //if (isInsideConvex([x, y], points)) {
+                if (isInsideGeneral([x, y], points)) {
                         ctx.fillStyle = "blue";
                 } else {
                         ctx.fillStyle = "red";
@@ -68,7 +95,7 @@ function point(event) {
                 if (points.length > 0) {
                         ctx.lineTo(x, y);
                 }
-		points.push([x, y]);
+                points.push([x, y]);
                 ctx.arc(x, y, r, 0, 2 * Math.PI, false);
                 ctx.fill();
                 ctx.moveTo(x, y);
@@ -82,12 +109,12 @@ function handleMode(event) {
 
                 const canvas = document.getElementById("canvas");
                 const ctx = canvas.getContext("2d");
-		const [x, y] = points[0];
-		ctx.lineTo(x, y);
+                const [x, y] = points[0];
+                ctx.lineTo(x, y);
                 ctx.closePath();
                 ctx.stroke();
 
-		points.push(points[0]);
+                points.push(points[0]);
         }
 }
 
